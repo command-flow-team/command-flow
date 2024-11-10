8af726e7d851dcf3e936eb3a7d0ef47289781f69
Bohdan Tyshchenko <whym4rsky@gmail.com>
2024-11-10 04:02:23 +0100

    CFLW-24

--- ccmd_mng.py
+++ ccmd_mng.py
@@ -5,13 +5,16 @@ class CommandCard(QWidget):
     def __init__(self, label_text, number, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
+        # set up layout for different cards
         layout = QVBoxLayout()
         
+        # test ccmd label set up
         self.label = QLabel(label_text)
         self.label.setAlignment(Qt.AlignCenter)
         self.label.setStyleSheet("font-size: 16px;")  # Increase font size for better readability
         layout.addWidget(self.label)
         
+        # test ccmd number set up
         self.number_label = QLabel(str(number))
         self.number_label.setAlignment(Qt.AlignCenter)
         self.number_label.setStyleSheet("font-size: 12px;")  # Increase font size
@@ -22,7 +25,7 @@ class CommandCard(QWidget):
         self.setStyleSheet("background-color: #151515; padding: 15px; color: white; font: 600 24pt \"Afacad Flux\";")  # Increase padding
         self.setFixedSize(140, 200)  # Adjust size as needed (width, height)
 
-class ccmd_widgets:
+class CcmdWidgets:
     def __init__(self, workzone):
         # main layout
         self.layout = QVBoxLayout()
@@ -36,44 +39,44 @@ class ccmd_widgets:
         self.scroll_widget.setLayout(self.layout)
         self.scroll_area.setWidget(self.scroll_widget)
         self.scroll_area.setStyleSheet('''
-        QScrollBar:vertical {
-             background: #141414;
-             width: 15px;
-             margin: 22px 0 22px 0;
-        }
-        QScrollBar::handle:vertical {
-            background: #757575;
-            min-height: 20px;
-            border-radius: 3px;
-        }
-        QScrollBar::add-line:vertical {
-            background: #141414;
-            height: 20px;
-            subcontrol-position: bottom;
-            subcontrol-origin: margin;
-        }
+            QScrollBar:vertical {
+                background: #141414;
+                width: 15px;
+                margin: 22px 0 22px 0;
+            }
+            QScrollBar::handle:vertical {
+                background: #757575;
+                min-height: 20px;
+                border-radius: 3px;
+            }
+            QScrollBar::add-line:vertical {
+                background: #141414;
+                height: 20px;
+                subcontrol-position: bottom;
+                subcontrol-origin: margin;
+            }
 
-        QScrollBar::sub-line:vertical {
-            background: #141414;
-            height: 20px;
-            subcontrol-position: top;
-            subcontrol-origin: margin;
-        }
-        QScrollBar::up-arrow:vertical, QScrollBar::down-arrow:vertical {
-            width: 3px;
-            height: 3px;
-            background: #757575;
-        }
+            QScrollBar::sub-line:vertical {
+                background: #141414;
+                height: 20px;
+                subcontrol-position: top;
+                subcontrol-origin: margin;
+            }
+            QScrollBar::up-arrow:vertical, QScrollBar::down-arrow:vertical {
+                width: 3px;
+                height: 3px;
+                background: #757575;
+            }
 
-        QScrollBar::add-page:vertical, QScrollBar::sub-page:vertical {
-            background: none;
-        }''')
+            QScrollBar::add-page:vertical, QScrollBar::sub-page:vertical {
+                background: none;
+            }''')
 
         # scroll area in the workzone
         workzone_layout = QVBoxLayout(workzone)
         workzone_layout.addWidget(self.scroll_area)
 
     def create_ccmd(self, label_text, number):
-        # Create an instance of CommandCard with label and number
+        # create an instance of CommandCard with label and number
         command_card = CommandCard(label_text, number)
         self.layout.addWidget(command_card)
